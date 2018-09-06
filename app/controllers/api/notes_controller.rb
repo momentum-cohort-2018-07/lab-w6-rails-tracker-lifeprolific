250f053ae3445107cec845a71e8d13a7a593ed67
lifeprolific <thegregoman@gmail.com>
2018-09-06 17:38:28 -0400

    nested notes routes and added actions

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -1,13 +1,30 @@
 class Api::NotesController < ApplicationController
   def index
+    @notes = FoodTruck.find(params[:food_truck_id]).notes
+    render json: @notes
   end
 
   def create
+    @note = Note.new(params.permit(:body, :food_truck_id))
+    if @note.save
+      render json: @note
+    end
   end
 
-  def delete
+  def destroy
+    @note = Note.find(params[:id])
+    if @note.delete
+      render :json => {
+        :status => :ok, 
+        :message => "Successfully deleted!",
+      }
+    end
   end
 
   def update
+    @note = Note.find(params[:id])
+    if @note.update(params.permit(:body, :food_truck_id))
+      render json: @note
+    end
   end
 end
